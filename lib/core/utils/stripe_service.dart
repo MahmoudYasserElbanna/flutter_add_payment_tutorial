02307276845a919441bd62eb9f28570bd58ec10a
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-10-22 15:13:04 +0300

    implement second stripe method

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -5,6 +5,7 @@
   [2] Init Payment Sheet.
   [3] Display/Present Payment Sheet.
   */
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:payment_app/core/utils/api_keys.dart';
 import 'package:payment_app/core/utils/api_service.dart';
 import 'package:payment_app/features/checkout/data/models/payment_intent_input_model.dart';
@@ -24,7 +25,17 @@ class StripeService {
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
   }
-  // TODO [2] Init Payment Sheet.
 
+  /* -------------------------------------------------------------------- */
+  // TODO [2] Init Payment Sheet.
+  Future initPaymentSheet({required String paymentIntentClientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        merchantDisplayName: 'Mahmoud Elbanna',
+        paymentIntentClientSecret: paymentIntentClientSecret,
+      ),
+    );
+  }
+  /* -------------------------------------------------------------------- */
   // TODO [3] Display/Present Payment Sheet.
 }
